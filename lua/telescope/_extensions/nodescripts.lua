a1551a0bd49bfefbdc962c56d2ec407189b1ad4b
Luis Simas <luissimas@protonmail.com>
2021-07-11 18:19:46 -0300

    Changing file structure and adding options

--- lua/telescope/_extensions/nodescripts.lua
+++ lua/telescope/_extensions/nodescripts.lua
@@ -1,9 +1,4 @@
-local json = require("telescope._extensions.json")
-local pickers = require("telescope.pickers")
-local actions = require("telescope.actions")
-local finders = require("telescope.finders")
-local sorters = require("telescope.sorters")
-local themes = require("telescope.themes")
+local main = require("telescope._extensions.nodescripts.main")
 
 local ok, telescope = pcall(require, "telescope")
 
@@ -11,56 +6,11 @@ if not ok then
   error("Nodescripts requires telescope")
 end
 
-function actions.run_command()
-  local script = actions.get_selected_entry()[1]
-
-  vim.cmd(":vsplit | terminal")
-  vim.cmd(':call jobsend(b:terminal_job_id, ["yarn run ' .. script .. '", ""])')
-end
-
-local function get_scripts()
-  local file = assert(io.open("package.json", "r"))
-  local package = json.decode(file:read("*a"))
-  local scripts = {}
-
-  for key, _ in pairs(package.scripts) do
-    -- Removing pre and post scripts
-    if not key:match("^pre") and not key:match("^post") then
-      table.insert(scripts, key)
-    end
-  end
-
-  return scripts
-end
-
-function run(opts)
-  opts = opts or {}
-  local scripts = get_scripts()
-
-  pickers.new(
-    themes.get_dropdown({}),
-    {
-      prompt_title = "Scrips",
-      finder = finders.new_table(scripts),
-      sorter = sorters.get_fzy_sorter(),
-      attach_mappings = function(_, map)
-        map("i", "<cr>", actions.run_command)
-        map("n", "<cr>", actions.run_command)
-        return true
-      end
-    }
-  ):find()
-end
-
-function reload()
-  package.loaded["telescope-node-scripts"] = nil
-end
-
 return telescope.register_extension(
   {
+    setup = main.setup,
     exports = {
-      run = run,
-      reload = reload
+      run = main.run
     }
   }
 )
---
+++ lua/telescope/_extensions/nodescripts/main.lua
@@ -0,0 +1,63 @@
+local json = require("telescope._extensions.nodescripts.json")
+local pickers = require("telescope.pickers")
+local actions = require("telescope.actions")
+local finders = require("telescope.finders")
+local sorters = require("telescope.sorters")
+local themes = require("telescope.themes")
+
+-- Config defaults
+local defaults = {
+  command = "npm run",
+  display_method = "vsplit"
+}
+
+local config
+
+local M = {}
+
+function actions.run_command()
+  local script = actions.get_selected_entry()[1]
+
+  vim.cmd(":" .. config.display_method .. " | terminal")
+  vim.cmd(":call jobsend(b:terminal_job_id, ['" .. config.command .. " " .. script .. "', ''])")
+end
+
+local function get_scripts()
+  local file = assert(io.open("package.json", "r"))
+  local package = json.decode(file:read("*a"))
+  local scripts = {}
+
+  for key, _ in pairs(package.scripts) do
+    -- Removing pre and post scripts
+    if not key:match("^pre") and not key:match("^post") then
+      table.insert(scripts, key)
+    end
+  end
+
+  return scripts
+end
+
+function M.run(opts)
+  opts = opts or {}
+  local scripts = get_scripts()
+
+  pickers.new(
+    themes.get_dropdown({}),
+    {
+      prompt_title = "Scrips",
+      finder = finders.new_table(scripts),
+      sorter = sorters.get_fzy_sorter(),
+      attach_mappings = function(_, map)
+        map("i", "<cr>", actions.run_command)
+        map("n", "<cr>", actions.run_command)
+        return true
+      end
+    }
+  ):find()
+end
+
+function M.setup(options)
+  config = vim.tbl_deep_extend("force", defaults, options or {})
+end
+
+return M
